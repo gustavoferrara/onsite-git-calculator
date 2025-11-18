98baff8dbee535a003ae4b27fe4d5632a3b5ae27
Diego Caplan <diegocaplan@gmail.com>
2025-11-18 10:18:50 -0600

    deliver issame function and comment added in divide

--- operators/isSame.js
+++ operators/isSame.js
@@ -1,9 +1,5 @@
 function isSame(a, b) {
-  /**
-   * Checks if the two numbers are equal. (TODO: Implement this)
-   */
-// --- YOUR CODE GOES HERE - MAKE THE FUNCTION RETURN THE RESULT WITH `return` ---
-
+  return a === b;
 }
 
 export default isSame;

b4defaaac2a779c81bda99e09aa6513ae8e9b9a8
peterflier <peterflier@gmail.com>
2025-11-18 08:16:15 -0800

    Updated max function

--- operators/add.js
+++ operators/add.js
@@ -1,9 +1,9 @@
 function add(a, b) {
-  /**
-   * Adds two numbers together.
-   */
-  // --- YOUR CODE GOES HERE - MAKE THE FUNCTION RETURN THE RESULT WITH `return` ---
-  
+    /**
+     * Adds two numbers together.
+     */
+    // --- YOUR CODE GOES HERE - MAKE THE FUNCTION RETURN THE RESULT WITH `return` ---
+    return a + b;
 }
 
 export default add;
--- operators/max.js
+++ operators/max.js
@@ -1,8 +1,9 @@
 function max(a, b) {
-  /**
-   * Finds the larger of the two numbers. (TODO: Implement this)
-   */
-// --- YOUR CODE GOES HERE - MAKE THE FUNCTION RETURN THE RESULT WITH `return` ---
+    /**
+     * Finds the larger of the two numbers. (TODO: Implement this)
+     */
+    // --- YOUR CODE GOES HERE - MAKE THE FUNCTION RETURN THE RESULT WITH `return` ---
+    return a > b ? a : b;
 }
 
 export default max;

4fd47076de8a1cf7694d080b55e53eb7a5cc0d7b
cristobalcastrillon <cristobalcastrillon@gmail.com>
2025-11-18 10:17:22 -0600

    Average function implemented.

--- operators/average.js
+++ operators/average.js
@@ -2,8 +2,8 @@ function average(a, b) {
   /**
    * Calculates the average of a and b. (TODO: Implement this)
    */
-  // --- YOUR CODE GOES HERE - MAKE THE FUNCTION RETURN THE RESULT WITH `return` ---
 
+  return (a + b) / 2;
 }
 
 export default average;

c5b5ceb8aae94bdbb18e951335817ce1122bba01
Mike Meesseman <mike.meesseman@spatialnetworks.com>
2025-11-18 10:20:42 -0600

    add max functionality

--- operators/max.js
+++ operators/max.js
@@ -2,7 +2,8 @@ function max(a, b) {
   /**
    * Finds the larger of the two numbers. (TODO: Implement this)
    */
-// --- YOUR CODE GOES HERE - MAKE THE FUNCTION RETURN THE RESULT WITH `return` ---
+  // Return the greater value (if equal, returns `a`)
+  return a >= b ? a : b;
 }
 
 export default max;
